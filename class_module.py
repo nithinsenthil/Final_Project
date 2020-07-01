23ba6ab08200eb1163d79f4f1771a83d36e19820
ShadowTechCraft <ShadowTechCraft@users.noreply.github.com>
2020-06-30 20:48:39 -0700

    Create general class structure

--- class_module.py
+++ class_module.py
@@ -1,1 +1,18 @@
 #The class(es) can be written here
+class Game:
+
+    def __init__(self):
+        pass
+
+
+class Input:
+
+    def __init__(self):
+        pass
+
+
+class computerInput:
+
+    def __init__(self):
+        pass
+
